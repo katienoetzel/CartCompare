3d8f78cdaeb77de70a460dc8486b93727c096204
klnoetze <klnoetze@ncsu.edu>
2026-09-12 22:47:19 -0400

    pushing rest of files from step 15

--- backend/src/CartCompare.Services/obj/Debug/net10.0/CartCompare.Services.AssemblyInfo.cs
+++ backend/src/CartCompare.Services/obj/Debug/net10.0/CartCompare.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartCompare.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b6eb5e9a8322304105aa03a40c5e77e605198f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c770f20efa8881ac2c8416e75c9042e8d5a7c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartCompare.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartCompare.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
